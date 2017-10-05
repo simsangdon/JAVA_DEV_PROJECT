f87a738ea9c265cecdfc12ecd3cb9bb3913a198c
simsangdon <hahrdhaly12@gmail.com>
2017-10-05 20:18:50 +0900

    파일 READ텍스트

--- first/src/main/java/first/common/util/file/fileTest.java
+++ first/src/main/java/first/common/util/file/fileTest.java
@@ -4,7 +4,9 @@ public class fileTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		fileUtil fUtil = new fileUtil();
+		String filePath = "src\\main\\java\\first\\common\\util\\file\\testFile\\testFile1";
+		fUtil.readFile(filePath);
 	}
 
 }
--- first/src/main/java/first/common/util/file/fileUtil.java
+++ first/src/main/java/first/common/util/file/fileUtil.java
@@ -1,7 +1,46 @@
 package first.common.util.file;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class fileUtil {
 
 	/* 라인별 파일 읽는 메소드 */
-	
+	public int readFile(String filePath) {
+		BufferedReader br = null;
+		StringBuffer sb = new StringBuffer();
+		
+		try 
+		{
+			br = new BufferedReader(new FileReader(filePath));
+			String line = null;
+			
+			while((line = br.readLine()) != null)
+			{
+				System.out.println(line);
+				sb.append(line);
+			}
+		} 
+		catch (IOException ioe) 
+		{
+			System.out.println(ioe.getMessage());
+		} 
+		finally 
+		{
+			try
+			{
+				if(br != null) 
+				{
+					br.close();
+				}
+			}
+			catch (Exception e)
+			{
+				e.getMessage();
+			}
+		}
+		
+		return sb.length();
+	}
 }
