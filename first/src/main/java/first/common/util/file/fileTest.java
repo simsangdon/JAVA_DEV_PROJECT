2ac893cc843d8afce6891c0000c19c972b9ef4db
simsangdon <hahrdhaly12@gmail.com>
2017-10-05 22:44:31 +0900

    파일인코딩 테스트

--- first/src/main/java/first/common/util/file/fileTest.java
+++ first/src/main/java/first/common/util/file/fileTest.java
@@ -1,12 +1,26 @@
 package first.common.util.file;
 
+import java.io.File;
+
 public class fileTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		fileUtil fUtil = new fileUtil();
-		String filePath = "src\\main\\java\\first\\common\\util\\file\\testFile\\testFile1";
-		fUtil.readFile(filePath);
+		String filePath = "src\\main\\java\\first\\common\\util\\file\\testFile\\testFile1-UTF-8";
+		//System.out.println(fUtil.readFile(filePath));
+		
+		File file = new File(filePath);
+		String encoding = "UTF-8";
+		
+		if(file.isDirectory())
+		{
+			fUtil.iterateDirectory(file, encoding);
+		}
+		else
+		{
+			fUtil.convertEncoding(file, encoding);
+		}
 	}
 
 }
--- first/src/main/java/first/common/util/file/fileUtil.java
+++ first/src/main/java/first/common/util/file/fileUtil.java
@@ -1,13 +1,22 @@
 package first.common.util.file;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class fileUtil {
 
-	/* 라인별 파일 읽는 메소드 */
-	public int readFile(String filePath) {
+	/*
+	 * 라인별 파일 읽는 메소드
+	 */
+	public int readFile(String filePath) 
+	{
 		BufferedReader br = null;
 		StringBuffer sb = new StringBuffer();
 		
@@ -42,5 +51,71 @@ public class fileUtil {
 		}
 		
 		return sb.length();
+	}
+	
+	/*
+	 * 대상 노드가 폴더인 경우
+	 */
+	public void iterateDirectory(File file, String encoding) 
+	{
+		try
+		{
+			String path = file.getCanonicalPath();
+			String [] str = file.list();
+			File resFile = null;
+			
+			for (String fileName : str) 
+			{
+				resFile = new File(path + "/" + fileName);
+				System.out.println("filePath : " + path + "/" + fileName);
+				if(resFile.isDirectory())
+				{
+					// 해당 노드가 폴더이면 제귀호출
+					iterateDirectory(resFile, encoding);
+				}
+				else
+				{
+					// 해당 노드가 파일이면 파일 처리 부분 호출
+					convertEncoding(resFile, encoding);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * 대상 노드가 파일인 경우
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void convertEncoding(File file, String encoding) 
+	{
+		try
+		{
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String read = null;
+			List list = new ArrayList();
+			
+			while((read = in.readLine()) != null)
+			{
+				list.add(read);
+			}
+			in.close();
+			
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+			for (Object object : list) {
+				out.write((String)object);
+				out.newLine();
+			}
+			out.close();
+		}
+		catch (IOException ioe)
+		{
+			ioe.getMessage();
+			ioe.getStackTrace();
+		}
 	}
 }
